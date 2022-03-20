cc72a7a88073475b7cd537d66959efe09e8ad85d
magicalbeauty91 <magicalbeauty91@gmail.com>
2022-03-20 18:42:18 -0500

    extracted week forecast

--- assets/js/script.js
+++ assets/js/script.js
@@ -6,7 +6,10 @@ var date = moment().format('L');
 let weather = {
     "apiKey": "b41dfd6635a9cbc6a27f2bc778fcecf5",
     fetchWeather: function(city) {
-        fetch("https://api.openweathermap.org/data/2.5/weather?q=" + city + "&units=imperial&appid=" + this.apiKey)
+        fetch("https://api.openweathermap.org/data/2.5/weather?q=" 
+        + city + "&units=imperial&appid=" 
+        + this.apiKey
+        )
         .then((response) => response.json())
         .then((data) => this.displayWeather(data));
     },
@@ -16,22 +19,30 @@ let weather = {
         const { temp, humidity } = data.main;
         const { speed } = data.wind;
         const { lon, lat } = data.coord;
+        const { id } = data;
         document.querySelector(".city").innerText = name + " " + date;
         document.querySelector(".icon").src = "http://openweathermap.org/img/wn/" + icon + "@2x.png";
         document.querySelector(".temp").innerText = "Temp: " + temp + "°F";
         document.querySelector(".humidity").innerText = "Humidity: " + humidity + "%";
         document.querySelector(".wind").innerText = "Wind: " + speed + " mph";
         this.fetchUVIndex(lat, lon)
+        this.fetchWeekForecast(id)
     },
-    "apiKey": "b41dfd6635a9cbc6a27f2bc778fcecf5",
+    "apiKey": "dabd7c25e91b1eb7eb72fee9a490fe03",
     fetchUVIndex: function(lat, lon) {
-        fetch("https://api.openweathermap.org/data/2.5/onecall?lat=" + lat + "&lon=" + lon + "&units=imperial&appid=" + this.apiKey)
+        fetch("https://api.openweathermap.org/data/2.5/onecall?lat=" 
+        + lat 
+        + "&lon=" 
+        + lon 
+        + "&units=imperial&appid=" 
+        + this.apiKey
+        )
         .then((response) => response.json())
         .then((data) => this.displayUVIndex(data));
     },
 
     displayUVIndex: function(data) {
-        const { uvi } = data.current; console.log(uvi)
+        const { uvi } = data.current;
         if (uvi <= 2.0) {
             document.querySelector(".index").innerText = uvi;
             document.querySelector(".index").classList = ("badge badge-success");
@@ -42,6 +53,17 @@ let weather = {
             document.querySelector(".index").innerText = uvi;
             document.querySelector(".index").classList = ("badge badge-danger");
         }
+    },
+
+    "apiKey": "17ff6c9a0c05f29340ecc08ff3599a7e",
+    fetchWeekForecast: function(id) {
+        fetch("http://api.openweathermap.org/data/2.5/forecast?id=" + id + "&cnt=5&appid=" + this.apiKey)
+        .then((response) => response.json())
+        .then((data) => this.displayWeekForecast(data));
+    },
+
+    displayWeekForecast: function(data) {
+        
     },
 
     search: function() {
