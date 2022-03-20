4e45d1e49439efe3ff7c821183cdd6bd83de8287
magicalbeauty91 <magicalbeauty91@gmail.com>
2022-03-19 21:15:22 -0500

    added functionality to search button to display weather info

--- assets/js/script.js
+++ assets/js/script.js
@@ -1,6 +1,7 @@
 $("#weather-info").hide();
 $("#recent-searches").hide();
-
+var date = moment().format('L');
+        
 
 let weather = {
     "apiKey": "b41dfd6635a9cbc6a27f2bc778fcecf5",
@@ -15,21 +16,27 @@ let weather = {
         const { temp, humidity } = data.main;
         const { speed } = data.wind;
         console.log(name,icon,description,temp,humidity,speed)
-    }
+        document.querySelector(".city").innerText = name + " " + date;
+        document.querySelector(".icon").src = "http://openweathermap.org/img/wn/" + icon + "@2x.png";
+        document.querySelector(".temp").innerText = "Temp: " + temp + "°F";
+        document.querySelector(".humidity").innerText = "Humidity: " + humidity + "%";
+        document.querySelector(".wind").innerText = "Wind: " + speed + " mph";
+        document.querySelector(".index")
+    },
+    search: function() {
+        this.fetchWeather(document.querySelector(".searched-city").value);
+    },
 };
 
 
-// on click event to search city
-$("#searchBtn").on("click", function(event) {
+document.querySelector(".search-btn").addEventListener("click", function(event) {
     event.preventDefault();
     $("#weather-info").show();
     $("#recent-searches").show();
+    weather.search();
+});
 
-    var searchedCityEl = $("searchedCity").val();
-    if (searchedCityEl === "") {
-        return;
-    }
-})
+
 
 
 
