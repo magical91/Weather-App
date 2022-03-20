a8fe4c394d29d1931817e32552553ba32e69d704
magicalbeauty91 <magicalbeauty91@gmail.com>
2022-03-20 15:22:28 -0500

    extracted uv index

--- assets/js/script.js
+++ assets/js/script.js
@@ -1,7 +1,7 @@
 $("#weather-info").hide();
 $("#recent-searches").hide();
 var date = moment().format('L');
-        
+
 
 let weather = {
     "apiKey": "b41dfd6635a9cbc6a27f2bc778fcecf5",
@@ -12,21 +12,38 @@ let weather = {
     },
     displayWeather: function(data) {
         const { name } = data;
-        const { icon, description } = data.weather[0];
+        const { icon } = data.weather[0];
         const { temp, humidity } = data.main;
         const { speed } = data.wind;
-        console.log(name,icon,description,temp,humidity,speed)
+        const { lon, lat } = data.coord;
         document.querySelector(".city").innerText = name + " " + date;
         document.querySelector(".icon").src = "http://openweathermap.org/img/wn/" + icon + "@2x.png";
         document.querySelector(".temp").innerText = "Temp: " + temp + "°F";
         document.querySelector(".humidity").innerText = "Humidity: " + humidity + "%";
         document.querySelector(".wind").innerText = "Wind: " + speed + " mph";
-        document.querySelector(".index")
+        fetchUVIndex(lat, lon)
     },
+    "apiKey": "b41dfd6635a9cbc6a27f2bc778fcecf5",
+    fetchUVIndex: function(lat, lon) {
+        fetch("https://api.openweathermap.org/data/2.5/onecall?lat=" + lat + "&lon=" + lon + "&units=imperial&appid=" + this.apiKey)
+        .then((response) => response.json())
+        .then((data) => this.displayUVIndex(data));
+    },
+
+    displayUVIndex: function(data) {
+        const { uvi } = data.current;
+        // if (uvi <= 2.0) {
+        //     document.querySelector(".index").innerText = "UV Index: " + uvi;
+        // }
+    },
+
     search: function() {
         this.fetchWeather(document.querySelector(".searched-city").value);
     },
 };
+
+
+
 
 
 document.querySelector(".search-btn").addEventListener("click", function(event) {
