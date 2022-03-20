8c620afcc843de1615a89d314ea57782daa741f5
magicalbeauty91 <magicalbeauty91@gmail.com>
2022-03-19 19:07:38 -0500

    added api key and fetch functionality to get weather forecast

--- assets/js/script.js
+++ assets/js/script.js
@@ -1,11 +1,35 @@
-var searchButton = document.querySelector(".btn")
-var weatherInfo = document.querySelector(".weather-info")
-var searchHistory = document.querySelector(".search")
+$("#weather-info").hide();
+$("#recent-searches").hide();
 
-searchButton.addEventListener("click", displayContent);
 
-function displayContent() {
-    event.preventDefault()
-    weatherInfo.classList.remove("hide");
-    searchHistory.classList.remove("hide");
-}
+let weather = {
+    "apiKey": "b41dfd6635a9cbc6a27f2bc778fcecf5",
+    fetchWeather: function(city) {
+        fetch("https://api.openweathermap.org/data/2.5/weather?q=" + city + "&units=imperial&appid=" + this.apiKey)
+        .then((response) => response.json())
+        .then((data) => this.displayWeather(data));
+    },
+    displayWeather: function(data) {
+        const { name } = data;
+        const { icon, description } = data.weather[0];
+        const { temp, humidity } = data.main;
+        const { speed } = data.wind;
+        console.log(name,icon,description,temp,humidity,speed)
+    }
+};
+
+
+// on click event to search city
+$("#searchBtn").on("click", function(event) {
+    event.preventDefault();
+    $("#weather-info").show();
+    $("#recent-searches").show();
+
+    var searchedCityEl = $("searchedCity").val();
+    if (searchedCityEl === "") {
+        return;
+    }
+})
+
+
+
