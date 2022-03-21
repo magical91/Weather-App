78911a4203d1714f219912516f127a7d68dc86e3
magicalbeauty91 <magicalbeauty91@gmail.com>
2022-03-20 20:13:04 -0500

    displayed week forecast on application

--- assets/js/script.js
+++ assets/js/script.js
@@ -1,6 +1,6 @@
 $("#weather-info").hide();
 $("#recent-searches").hide();
-var date = moment().format('L');
+var currentDate = moment().format('L');
 
 
 let weather = {
@@ -20,11 +20,11 @@ let weather = {
         const { speed } = data.wind;
         const { lon, lat } = data.coord;
         const { id } = data;
-        document.querySelector(".city").innerText = name + " " + date;
+        document.querySelector(".city").innerText = name + " " + currentDate;
         document.querySelector(".icon").src = "http://openweathermap.org/img/wn/" + icon + "@2x.png";
         document.querySelector(".temp").innerText = "Temp: " + temp + "°F";
         document.querySelector(".humidity").innerText = "Humidity: " + humidity + "%";
-        document.querySelector(".wind").innerText = "Wind: " + speed + " mph";
+        document.querySelector(".wind").innerText = "Wind: " + speed + " MPH";
         this.fetchUVIndex(lat, lon)
         this.fetchWeekForecast(id)
     },
@@ -57,13 +57,41 @@ let weather = {
 
     "apiKey": "17ff6c9a0c05f29340ecc08ff3599a7e",
     fetchWeekForecast: function(id) {
-        fetch("http://api.openweathermap.org/data/2.5/forecast?id=" + id + "&cnt=5&appid=" + this.apiKey)
+        fetch("http://api.openweathermap.org/data/2.5/forecast?id=" + id + "&cnt=5&units=imperial&appid=" + this.apiKey)
         .then((response) => response.json())
         .then((data) => this.displayWeekForecast(data));
     },
 
     displayWeekForecast: function(data) {
-        
+        var cardHTML = "";
+        for (var i = 0; i < data.list.length; i++) {
+            var weatherIcon = data.list[i].weather[0].icon;console.log(dateStrArr)
+		    var dateStr = data.list[i].dt_txt;console.log(dateStr)
+		    var dateStrArr = dateStr.split(" ");
+		    var date = dateStrArr[0];
+		    var dateArr = date.split("-");
+		    var newDate = dateArr[1] + "/" + dateArr[2] + "/" + dateArr[0];
+            cardHTML += `
+			    <div class="card text-white bg-info p-1 mr-3">
+				    <div class="card-header text-center font-weight-bold">${newDate}</div>
+				    <div class="card-body">
+				        <p class="card-text text-center">
+					        <img id="weather-icon" src="https://openweathermap.org/img/wn/${weatherIcon}.png"/>
+				        </p>
+				        <p class="card-text">
+					        Temp: ${data.list[i].main.temp} °F
+				        </p>
+                        <p class="card-text">
+                            Wind: ${data.list[i].wind.speed} MPH
+                        </p>
+				        <p class="card-text">
+					        Humidity: ${data.list[i].main.humidity}%
+				        </p>
+				    </div>
+			    </div>`;
+  
+		    document.querySelector(".city-week-forecast").innerHTML = cardHTML;
+        }
     },
 
     search: function() {
